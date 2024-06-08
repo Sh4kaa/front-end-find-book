99dd1e8b1200e65e2d138b45a0420a863914483b
Dario de souza <darioo_dario@hotmail.com>
2024-06-07 21:20:49 -0300

    chore: assets do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-green-500">Hello World</h1>
+      <h1 className="text-evergreen-light">Hello World</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        evergreen: '#2B4341',
+        'evergreen-light': '#2AA78D'
+
+      },
+    },
   },
   plugins: [],
 }
