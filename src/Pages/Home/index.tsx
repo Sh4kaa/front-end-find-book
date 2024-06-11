09bbdd5910d9dbc07797ca53ae80f90210ebeb24
Dario de souza <darioo_dario@hotmail.com>
2024-06-11 12:08:26 -0300

    feature: selecionar generos

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,16 +3,36 @@ import { Header } from '../../components/HeaderTitle/header'
 import { Container } from '../../components/Container/container'
 import { Title } from '../../components/Title/title'
 import Button from '../../components/Button/button'
+import { useCallback, useState } from 'react'
 
 const genderBooks = ['Ação', 'Drama', 'Ficção', 'Terror', 'Biografia', 'Aventura']
 
 export const Home = () => {
+  const [selectedGender, setSelectedGender] = useState<string[]>([])
+
+  const handleSelect = useCallback((gender: string) => {
+    if (selectedGender.includes(gender)) {
+      const removeGender = selectedGender.filter(item => item !== gender)
+      setSelectedGender(removeGender)
+    } else {
+      setSelectedGender([...selectedGender, gender])
+    }
+  }, [selectedGender])
+
   return (
     <>
       <Header />
       <Container>
         <Title>O que você quer ler hoje</Title>
-        {genderBooks.map(book => <Button variant="dark">{book}</Button>)}
+        {genderBooks.map(gender => (
+          <Button
+            key={gender}
+            variant={selectedGender.includes(gender) ? 'light' : 'dark'}
+            onClick={() => handleSelect(gender)}
+          >
+            {gender}
+          </Button>
+        ))}
       </Container>
     </>
   )
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -5,14 +5,15 @@ type ButtonProps = React.ComponentProps<'button'> & {
   variant?: 'light' | 'dark'
 }
 
-const Button = ({ children, variant = 'dark', className }: ButtonProps) => {
+const Button = ({ children, variant = 'dark', className, ...rest }: ButtonProps) => {
 
   return (
     <button
-      className={classNames(['bg-evergreen-light text-white px-6 py-2 rounded-lg shadow font-medium mt-3',
+      className={classNames(['bg-evergreen-light px-6 py-3 shadow text-white font-medium rounded-lg mt-3',
         variant === 'dark' && 'bg-evergreen-light',
         variant === 'light' && 'bg-white border-2 border-evergreen-light text-evergreen-light', className
       ])}
+      {...rest}
     >
       {children}
     </button>
